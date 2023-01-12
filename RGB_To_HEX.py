b8c1427132d02a4e3c64a2348f55d050897107ae
Seth-SH <62153326+Kash-001@users.noreply.github.com>
2023-01-12 09:11:06 +0100

    Finished script update

--- RGB_To_HEX.py
+++ RGB_To_HEX.py
@@ -1,4 +1,20 @@
 def rgb(r, g, b):
-    r = hex(r)
-    g = hex(g)
-    b = hex(b)
+    vals = [r,g,b]
+    cnt = 0
+    for v in vals:
+        if v > 255:
+            vals[cnt] = 255
+        cnt += 1
+    cnt = 0
+    for v in vals:
+        if str(v).isdigit():
+            if v != 0: 
+                vals[cnt] = hex(v).lstrip("0x").rstrip("L")
+                if len(vals[cnt]) != 2:
+                    vals[cnt] = '0'+vals[cnt]
+            else:
+                vals[cnt] = '00' 
+        else:
+            vals[cnt] = '00'
+        cnt += 1
+    return ''.join(str(v) for v in vals).upper()
