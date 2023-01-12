f8d360030b31c3fb50887d4ba50bb5cd126be59d
Seth-SH <62153326+Kash-001@users.noreply.github.com>
2023-01-12 09:37:14 +0100

    final code update

--- The_HashTag_Generator.py
+++ The_HashTag_Generator.py
@@ -1,1 +1,10 @@
-
+def generate_hashtag(s: str):
+    if s.strip() == '':
+        return False
+    words = s.split(' ')
+    if len(words) <= 0:
+        return False
+    for w in range(len(words)):
+        words[w] = words[w].capitalize()
+    final = '#'+''.join(str(w) for w in words)
+    return final if len(final) < 140 else False
