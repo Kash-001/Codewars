1bb0ee030019ef0fdb45416cf4bc0234777efeda
Seth-SH <62153326+Kash-001@users.noreply.github.com>
2023-01-12 14:47:03 +0100

    Final release

--- Is_Isogram.py
+++ Is_Isogram.py
@@ -1,5 +1,5 @@
-def is_isogram(string):
-    a = 0
+def is_isogram(string: str):
+    a, string = 0, string.upper()
     for s in string:
-        a += string.count(s)
+        a = a + string.count(s)
     return True if a == len(string) else False
