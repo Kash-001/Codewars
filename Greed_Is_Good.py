6f6a93af19c3e854e8a038e3e8ca20604610d076
Seth-SH <62153326+Kash-001@users.noreply.github.com>
2023-01-12 12:29:39 +0100

    Final script release

--- Greed_Is_Good.py
+++ Greed_Is_Good.py
@@ -1,1 +1,22 @@
-
+def score(dice: list):
+    score = 0
+    while len(dice) != 0:
+        for d in dice:
+            if dice.count(d) == 3:
+                if d == 1:
+                    score += d*1000
+                    for i in range(3):
+                        dice.remove(d)
+                else:   
+                    score += d*100
+                    for i in range(3):
+                        dice.remove(d)
+            elif d == 1:
+                score += 100
+                dice.remove(d)
+            elif d == 5:
+                score += 50
+                dice.remove(d)
+            else: 
+               dice.remove(d)
+    return score
