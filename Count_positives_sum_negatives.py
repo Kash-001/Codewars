a955636b9a4d7bca572dccf14522b33926cc0a4c
Seth-SH <62153326+Kash-001@users.noreply.github.com>
2023-01-16 16:27:02 +0100

    Create Count_positives_sum_negatives.py

---
+++ Count_positives_sum_negatives.py
@@ -0,0 +1,10 @@
+def count_positives_sum_negatives(arr: list):
+    rets = [0,0]
+    for a in arr:
+        if a > 0:
+            rets[0] += 1
+        elif a < 0:
+            rets[1] += a
+        else:
+            pass
+    return rets if arr != [] else []
